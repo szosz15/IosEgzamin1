58ccbcf3d6f132ee8c69ffa74553f95be3b94182
Foxu97 <kolo1908@gmail.com>
2020-06-26 12:56:52 +0200

    works kurwa

--- iosExam/App.js
+++ iosExam/App.js
@@ -1,12 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import md5 from 'md5'
 
 export default function App() {
+  const [name, setName] = React.useState('');
+  const [surname, setSurname] = React.useState('');
+  const [generatedMD5, setGeneratedMD5] = React.useState('');
+  const [resultFromApi, setResultFromApi] = React.useState('');
+
+  const apiCall = async (md5String) => {
+    try {
+      console.log("md5string",md5String)
+      const url = `http://sroczynski.pl/iosexamrest/examresult/${md5String}`;
+      console.log("url", url)
+      const response = await fetch(url);
+      const resJson = await response.json();
+      console.log("json", resJson)
+      setResultFromApi(resJson.result)
+
+    }
+    catch (err) {
+      console.log("error")
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <Text>Enter your name</Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setName(text)}
+      value={name}
+    />
+<Text style={{ marginTop: 12}}>Enter your surname</Text>
+<TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => setSurname(text)}
+      value={surname}
+    />
+      </View>
+      <Button
+  onPress={() => {
+    const string = `${surname} ${name}`;
+    console.log(string)
+    const generateMD5String = md5(`${surname} ${name}`)
+    setGeneratedMD5(generateMD5String);
+    apiCall(generateMD5String)
+  }}
+  title="Generate MD5"
+  color="#841584"
+/>
+<View style={{ marginTop: 24}}>
+      <Text>Api response: </Text>
+      <Text>{resultFromApi}</Text>
+      </View>
     </View>
   );
 }
@@ -18,4 +66,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inputContainer: {
+    width: '80%'
+  }
 });
